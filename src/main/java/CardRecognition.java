9c41a4c8d3432a26f7e892946dafbb506eb767f4
Ivan Lastouski <redbringers@gmail.com>
2022-07-05 20:37:36 +0300

    commit

--- src/main/java/CardRecognition.java
+++ src/main/java/CardRecognition.java
@@ -9,19 +9,18 @@ public class CardRecognition {
     public static void main(String[] args) {
         long time = System.currentTimeMillis();
         Recognition recognition;
-        //final File dir = new File(args[0]);
-        final File dir = new File("source/");
+        final File dir = new File(args[0]);
         if(dir.isDirectory())
         {
             for(File item : dir.listFiles(ImageFilter.IMAGE_FILTER)){
-               BufferedImage image = null;
+               BufferedImage bufferedImage = null;
                 try {
-                    image = ImageIO.read(item);
-                } catch (final IOException e) {
+                    bufferedImage = ImageIO.read(item);
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
                 StringBuilder results = new StringBuilder();
-                recognition = new Recognition(image);
+                recognition = new Recognition(bufferedImage);
                 for (int i=0; i<5; i++) {
                     results.append(recognition.getRankOrSuit(i, 'r'))
                                 .append(recognition.getRankOrSuit(i, 's'));
--- src/main/java/Recognition.java
+++ src/main/java/Recognition.java
@@ -9,39 +9,39 @@ import java.util.Arrays;
 
 public class Recognition {
     private final BufferedImage img;
-    private final int[] topLeftCornerX = {149,219,289,364,439}; //Индексы колонок верхнего левого угла фрагмента матрицы изображения
-    private final int[] topLeftCornerY = {589, 617}; //Индексы рядов верхнего левого угла фрагмента матрицы изображения
-    private final int[] rankSize = {41, 31}; //размер фрагмента изображения значимости карты
-    private final int[] suitSize = {23, 21}; //размер фрагмента изображения масти карты
+    private final int[] topLeftCornerX = {149,219,289,364,439};
+    private final int[] topLeftCornerY = {589, 617};
+    private final int[] rankSize = {41, 31};
+    private final int[] suitSize = {23, 21};
 
     public Recognition(BufferedImage img) {
         this.img = img;
     }
 
-    public String getRankOrSuit(int number, char type) { //функция, которая возвращает значение значимости
+    public String getRankOrSuit(int number, char type) {
         String rankSuit = "";
         boolean isCard = false;
-        BufferedImage img1 = null;
+        BufferedImage getImage = null;
         if (type == 'r') {
-            img1 = img.getSubimage(topLeftCornerX[number], topLeftCornerY[0], rankSize[0], rankSize[1]); // взятие области значимости в картинке
+            getImage = img.getSubimage(topLeftCornerX[number], topLeftCornerY[0], rankSize[0], rankSize[1]);
         } else if (type == 's') {
-            img1 = img.getSubimage(topLeftCornerX[number], topLeftCornerY[1], suitSize[0], suitSize[1]); // взятие области масти в картинке
+            getImage = img.getSubimage(topLeftCornerX[number], topLeftCornerY[1], suitSize[0], suitSize[1]);
         }
         boolean isGray = false;
 
-        if (img1 != null && img1.getRGB(img1.getWidth() - 1, img1.getHeight() - 1) == -8882056) { //если последний пиксель - это серый пиксель (значение -8882056)
-            isGray = true; //то это изображение с серым покрытием
-            img1 = getBinaryImage(img1, 's'); //надо конвертировать в бинарное изображение
+        if (getImage != null && getImage.getRGB(getImage.getWidth() - 1, getImage.getHeight() - 1) == -8882056) {
+            isGray = true;
+            getImage = getBinaryImage(getImage, 's');
         }
 
-        String originalString =  getImageAsString(img1); //преобразование значимости/масти данной карты в бинарную строку
+        String originalString =  getImageAsString(getImage);
 
-        if (number > 2) { //если это место четвертой или пятой карты
-            Color c = new Color(img1.getRGB(img1.getWidth()-1, img1.getHeight()-1)); //получаем значение пикселя (R - красная полоса, G - зеленая полоса, B - синяя полоса)
-            isCard = isFourThorFifthCard(c.getRed(), c.getGreen(), c.getRed()); //есть ли четвертая или пятая карта
+        if (number > 2) {
+            Color c = new Color(getImage.getRGB(getImage.getWidth()-1, getImage.getHeight()-1));
+            isCard = isFourThorFifthCard(c.getRed(), c.getGreen(), c.getRed());
         }
 
-        if (number < 3 || isCard) { //если это одна из первых трех карт или есть тоже четвертая или пятая карта
+        if (number < 3 || isCard) {
             File dir = null;
             if (type == 'r') {
                 dir = new File("src/main/resources/image/ranks/"); //13 значений
@@ -60,11 +60,11 @@ public class Recognition {
                 } catch (IOException e) {
                     System.out.println("Картинку " + imgFile.getName() + " невозможно открыть!");
                 }
-                String targetString =  getImageAsString(target); //значимость как бинарная строка
+                String targetString =  getImageAsString(target);
 
-                int levenshtein = levenshtein(originalString, targetString, 1000); //расстояние Левенштейна
+                int levenshtein = levenshtein(originalString, targetString, 1000);
 
-                if (levenshtein < min) { //поиск минимума расстояния Левенштейна
+                if (levenshtein < min) {
                     min = levenshtein;
                     rankSuit = imgFile.getName().replaceFirst("[.][^.]+$", "");
                 }
@@ -73,62 +73,58 @@ public class Recognition {
         return rankSuit;
     }
 
-    public boolean isFourThorFifthCard(int r, int g, int b) { //Есть ли четвертая или пятая карта
-        //если это белый или серый пиксель
-        return (r == 255 && g == 255 && b == 255) || (r == 120 && g == 120 && b == 120); //то есть карта
+    public boolean isFourThorFifthCard(int r, int g, int b) {
+        return (r == 255 && g == 255 && b == 255) || (r == 120 && g == 120 && b == 120);
     }
 
-    public int levenshtein(String source, String target, int threshold) { //функция расстояния Левенштейна
+    public int levenshtein(String source, String target, int threshold) {
 
-        int N1 = source.length(); //длина строки символов изображения значимости или масти
-        int N2 = target.length(); //длина строки символо изображения-шаблона
+        int N1 = source.length();
+        int N2 = target.length();
 
-        int[] p = new int[N1 + 1]; //'предыдущий' массив
-        int[] d = new int[N1 + 1]; //'текущий' массив
-        int[] temp; //для помощи в замене p и d
+        int[] p = new int[N1 + 1];
+        int[] d = new int[N1 + 1];
+        int[] temp;
 
-        final int boundary = Math.min(N1, threshold) + 1; //заполняем значения начальной таблицы
+        final int boundary = Math.min(N1, threshold) + 1;
         for (int i = 0; i < boundary; i++) {
             p[i] = i;
         }
 
-        Arrays.fill(p, boundary, p.length, Integer.MAX_VALUE); //значение над самой правой записью будет проигнорировано в следующих итерациях цикла
+        Arrays.fill(p, boundary, p.length, Integer.MAX_VALUE);
         Arrays.fill(d, Integer.MAX_VALUE);
 
-        for (int j = 1; j <= N2; j++) { //проходит через t
-            char t_j = target.charAt(j - 1); //j-й символ t
+        for (int j = 1; j <= N2; j++) {
+            char t_j = target.charAt(j - 1);
             d[0] = j;
 
-            int min = Math.max(1, j - threshold); //вычислить индексы, ограничить размер массива
+            int min = Math.max(1, j - threshold);
             int max = (j > Integer.MAX_VALUE - threshold) ? N1 : Math.min(N1, j + threshold);
 
             if (min > max) {
                 return -1;
             }
 
-            if (min > 1) { //игнорировать запись слева от крайнего левого угла
+            if (min > 1) {
                 d[min - 1] = Integer.MAX_VALUE;
             }
 
-            for (int i = min; i <= max; i++) { //выполняет итерацию [min, max] в строке символов "source"
+            for (int i = min; i <= max; i++) {
                 d[i] = (source.charAt(i - 1) == t_j) ? p[i - 1] : 1 + Math.min(Math.min(d[i - 1], p[i]), p[i - 1]);
-                //по диагонали влево и вверх
-                //1 + минимум от ячейки слева, вверх, по диагонали влево и вверх
             }
-            // копировать текущие значения расстояний
+
             temp = p;
             p = d;
             d = temp;
         }
 
-        if (p[N1] <= threshold) { //если p[n] больше порога, нет гарантии, что это правильное расстояние
+        if (p[N1] <= threshold) {
             return p[N1];
         }
         return -1;
     }
 
-    public String getImageAsString(BufferedImage symbol) { //преобразование изображения в бинарную строку
-
+    public String getImageAsString(BufferedImage symbol) {
         short whiteBg = -1;
         StringBuilder binaryString = new StringBuilder();
         for (short y = 1; y < symbol.getHeight(); y++) {
@@ -138,24 +134,20 @@ public class Recognition {
             }
         }
         return binaryString.toString();
-        //то есть строку, где черному пикселю соответствует '*', а белому — пробел.
     }
-    public BufferedImage getBinaryImage(BufferedImage img, char type) { //конвертация изображения в бинарное
+    public BufferedImage getBinaryImage(BufferedImage img, char type) {
         for (int i = 0; i< img.getWidth(); i++) {
             for (int j = 0; j< img.getHeight(); j++) {
-                if (type == 's') { //если это изображение значимости или масти карты
-                    if (img.getRGB(i, j) == -8882056) { //если это серый пиксель
-                        img.setRGB(i, j, -1); //сохранение как белого пикселя
+                if (type == 's') {
+                    if (img.getRGB(i, j) == -8882056) {
+                        img.setRGB(i, j, -1);
                     } else {
-                        img.setRGB(i, j, 0); //все остальные пиксели соранить как черные
+                        img.setRGB(i, j, 0);
                     }
                 }
-                else if (type == 't') { //если это изображение-шаблон
-                    if (img.getRGB(i, j) != -1) {
-                        img.setRGB(i, j, 0); //если пиксель не белый, сохранить как черный
-                    }
+                else if (type == 't' && img.getRGB(i, j) != -1) {
+                    img.setRGB(i, j, 0);
                 }
-
             }
         }
         return img;
